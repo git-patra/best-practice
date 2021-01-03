0558e78dec6845261769d2eb5b352b55e4da478a
Patra <git-patra@github.com>
2021-01-03 12:10:05 +0700

    Make route login and JWT Token

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('login', 'Api\UserController@login');
+Route::post('register', 'Api\UserController@register');
